9fdc71c91aa135c9031dd080fade2520f6f3ac8b
KilnOfTheSecondFlame <pbauma@gmx.net>
2018-11-23 20:14:05 +0100

    Have better ELEVATION_ON_ERROR
    -0.0d == 0.0d, this is obviously not as desired

--- commonlib/src/main/java/commonlib/location/LocationFilterElevation.java
+++ commonlib/src/main/java/commonlib/location/LocationFilterElevation.java
@@ -1,7 +1,7 @@
 package commonlib.location;
 
 public interface LocationFilterElevation {
-    double ELEVATION_ON_ERROR = -0d;
+    double ELEVATION_ON_ERROR = -Double.MAX_VALUE;
 
     /**
      * Returns the altitude/elevation for a given GPS location.
